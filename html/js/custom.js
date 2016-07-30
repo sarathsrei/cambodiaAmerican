eeb28116650b81a7174943f77f4075fcfe7b0cb0
Stevens-Contractor, Sarath <Sarath.Stevens-Contractor@adp.com>
2016-07-29 21:48:27 -0400

    my update

---
+++ html/js/custom.js
@@ -0,0 +1,9 @@
+/* Theme Name:iDea - Clean & Powerful Bootstrap Theme
+ * Author: 
+ * Author URI:http://www.cambodia-ga.org
+ * Author e-mail:cambodia-ga.org@gmail.com
+ * Version: 1.3
+ * Created:October 2014
+ * License URI:http://support.wrapbootstrap.com/
+ * File Description: Place here your custom scripts
+ */
